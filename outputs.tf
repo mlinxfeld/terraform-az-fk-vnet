9d218cb44b7bf3c3ff2d89b59291f68d92ee9a05
mlinxfeld <martin.linxfeld@foggykitchen.com>
2025-12-23 13:30:51 +0100

    Added outputs.tf for subnets

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,21 @@ output "subnet_ids" {
     for k, s in azurerm_subnet.this : k => s.id
   }
 }
+
+output "subnet_address_prefixes" {
+  description = "Map of subnet name => list of address prefixes (CIDRs)."
+  value = {
+    for k, s in azurerm_subnet.this : k => s.address_prefixes
+  }
+}
+
+output "subnets" {
+  description = "Map of subnet name => subnet attributes (id, name, address_prefixes)."
+  value = {
+    for k, s in azurerm_subnet.this : k => {
+      id               = s.id
+      name             = s.name
+      address_prefixes = s.address_prefixes
+    }
+  }
+}
